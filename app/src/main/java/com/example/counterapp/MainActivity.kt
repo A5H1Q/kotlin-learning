5af7a026043adfcf926a36683b0d0258f5eb3310
A5H1Q <ashiksaleem20@gmail.com>
2023-05-23 12:08:13 +0530

    Functionality Implemented.

--- app/src/main/java/com/example/counterapp/MainActivity.kt
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.counterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private var counter = 0
+    private lateinit var counterTextView:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        counterTextView = findViewById(R.id.counter)
+        val myFab:FloatingActionButton = findViewById(R.id.floatingActionButton)
+        myFab.setOnClickListener {
+            counter++
+            counterTextView.text= getString(R.string.counter_text, counter)
+        }
     }
 }
