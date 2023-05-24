4c80ec347bd455b0526836ff2b1756a50cba956d
A5H1Q <ashiksaleem20@gmail.com>
2023-05-24 18:26:33 +0530

    Functionality Implemented. Bug Fix needed.

--- app/src/main/java/com/example/counterapp/MainActivity.kt
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.counterapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var noteAdapter: NoteAdapter
-    private lateinit var noteList: List<Note>
     private lateinit var emptyNotesTxtView : TextView
 
+    companion object {
+        lateinit var noteList: MutableList<Note>
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,17 +36,26 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = noteAdapter
 
         updateEmptyNotesVisibility()
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this, NewNote::class.java)
+            intent.putExtra("isNew", true)
+            launcher.launch(intent)
+        }
     }
-
-    private fun generateDummyNoteList(): List<Note> {
-        val notes = mutableListOf<Note>()
-        // Add dummy notes to the list
-        notes.add(Note("1", "This is my first note"))
-        notes.add(Note("2", "Lorem ipsum dolor sit Amet adispicing se mi amor jedor sil vou play"))
-        notes.add(Note("3", "Yeea ho"))
-        return notes
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        Log.d("wtf", noteList.toString())
+        noteAdapter = NoteAdapter(noteList)
+        recyclerView.adapter = noteAdapter
     }
-
+private fun generateDummyNoteList(): MutableList<Note> {
+    val notes = mutableListOf<Note>()
+    // Add dummy notes to the list
+    notes.add(Note("1", "This is my first note"))
+    notes.add(Note("2", "Lorem ipsum dolor sit Amet adispicing se mi amor jedor sil vou play"))
+    notes.add(Note("3", "Yeea ho"))
+    return notes
+}
     private fun updateEmptyNotesVisibility(){
         if (noteList.isEmpty()) {
             emptyNotesTxtView.visibility = View.VISIBLE
@@ -65,10 +80,16 @@ class NoteAdapter(private val notes: List<Note>) : RecyclerView.Adapter<NoteAdap
     override fun getItemCount(): Int {
         return notes.size
     }
-
     inner class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val previewTextView: TextView = itemView.findViewById(R.id.titleTextView)
-
+        init {
+            itemView.setOnClickListener {
+                val intent = Intent(itemView.context, NewNote::class.java)
+                intent.putExtra("isNew", false)
+                intent.putExtra("noteId", notes[adapterPosition].id)
+                itemView.context.startActivity(intent)
+            }
+        }
         fun bind(note: Note) {
             previewTextView.text = note.content.substring(0, minOf(note.content.length, 100))
         }
---
+++ app/src/main/java/com/example/counterapp/NewNote.kt
@@ -0,0 +1,78 @@
+package com.example.counterapp
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import com.example.counterapp.MainActivity.Companion.noteList
+
+class NewNote : AppCompatActivity() {
+    private lateinit var noteId: String
+    private lateinit var multiTextView: EditText
+    private lateinit var saveBtn: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_note)
+        val isNew: Boolean = intent.getBooleanExtra("isNew", true)
+
+        supportActionBar?.title = if (isNew) "New Note" else "Edit Note"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        multiTextView = findViewById(R.id.multiTextView)
+        saveBtn = findViewById(R.id.savebtn)
+
+
+        noteId = intent.getStringExtra("noteId") ?: ""
+        if(noteId != "") getMatchingText(noteId)
+        saveBtn.setOnClickListener {
+            if(isNew) addNote(multiTextView.text.toString()) else updateNote(noteId)
+        }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun addNote(txt:String){
+        noteList.add(Note(generateRandomText(), txt))
+        finish()
+    }
+    private fun updateNote(id: String){
+        Log.d("wtf","hello")
+        var allNotes= noteList
+        for ((i, note) in allNotes.withIndex()) {
+            if (note.id == id) {
+                allNotes[i] = Note(id,multiTextView.text.toString())
+                break
+            }
+        }
+        noteList=allNotes
+        setResult(Activity.RESULT_OK)
+        finish()
+    }
+    private fun getMatchingText(id: String){
+        for (note in noteList) {
+            if (note.id == id) {
+                multiTextView.setText(note.content)
+                break
+            }
+        }
+    }
+    private fun generateRandomText(): String {
+        val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9') // Define the allowed characters
+        return (1..5)
+            .map { allowedChars.random() } // Generate a random character from the allowed characters
+            .joinToString("") // Join the characters into a string
+    }
+
+}
