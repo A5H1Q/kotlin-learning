1c64412cae3322ed2dd02c7a5a14c511b00d8f8d
A5H1Q <ashiksaleem20@gmail.com>
2023-06-07 12:00:46 +0530

    GET Request.

--- app/src/main/java/com/example/counterapp/MainActivity.kt
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -3,22 +3,59 @@ package com.example.counterapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatDelegate
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import kotlinx.coroutines.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-    private var counter = 0
-    private lateinit var counterTextView:TextView
+    private lateinit var counterTextView: TextView
+    private val mainScope = MainScope()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main)
 
         counterTextView = findViewById(R.id.counter)
-        val myFab:FloatingActionButton = findViewById(R.id.floatingActionButton)
-        myFab.setOnClickListener {
-            counter++
-            counterTextView.text= getString(R.string.counter_text, counter)
+
+        val exFab: ExtendedFloatingActionButton = findViewById(R.id.fab)
+        exFab.setOnClickListener {
+            mainScope.launch {
+                val response = withContext(Dispatchers.IO) {
+                    sendGetRequest("https://www.thiswebsitewillselfdestruct.com/api/get_letter") // Replace with your URL
+                }
+                counterTextView.text = response
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mainScope.cancel() // Cancel the coroutine when the activity is destroyed
+    }
+
+    private fun sendGetRequest(urlString: String): String {
+        val url = URL(urlString)
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "GET"
+
+        val responseCode = connection.responseCode
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            val reader = BufferedReader(InputStreamReader(connection.inputStream))
+            val response = StringBuilder()
+
+            var line: String? = reader.readLine()
+            while (line != null) {
+                response.append(line)
+                line = reader.readLine()
+            }
+
+            reader.close()
+            return response.toString()
+        } else {
+            throw Exception("HTTP GET request failed with response code: $responseCode")
         }
     }
 }
